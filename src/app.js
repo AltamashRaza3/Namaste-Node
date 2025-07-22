68f525aee0249850fb8c726513e52a1f4ba59b21
Altamash Raza <altamashraza3.ar@gmail.com>
2025-07-23 01:54:50 +0530

    Exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -3,17 +3,28 @@ const express = require('express');
 //Creating a new express.js application
 const app = express();
 
-app.get("/",(req,res)=>{
-  res.send("Namaste from the Dashboard");
-})
+// This will handle GET call
+ app.get("/user",(req,res)=>{
+  res.send({firstName: "Altamash",lastName:"Raza"})
+});
 
-app.get("/test",(req,res)=>{
+app.post("/user",(req,res)=> {
+  res.send("Data Scuccessfully saved to the Database");
+})
+app.delete("/user",(req,res)=>{
+  res.send("Data is successfully deleted");
+}) 
+// this will match all the HTTP method API call to /test
+app.use("/test",(req,res)=>{
   res.send("Hello from the server");
 })
 
-app.get("/hello",(req,res)=>{
-  res.send("Hello Hello Hello");
-})
+// app.get("/",(req,res)=>{
+//   res.send("Namaste from the Dashboard");
+// })
+// app.get("/hello",(req,res)=>{
+//   res.send("Hello Hello Hello");
+// })
 
 app.listen(3000,()=>{
   console.log("Server is successfully listining on Port 3000");
