04ebb1489559e318ed455ad0a3356bfae25067f6
Altamash Raza <altamashraza3.ar@gmail.com>
2025-07-25 12:18:59 +0530

    create a new directory  chapter 02

--- src/app.js
+++ src/app.js
@@ -2,19 +2,26 @@ const express = require('express');
 
 const app= express();
 
+// Handle Auth Middleware for all GET,POST,.... request
+app.use("/admin",(req,res,next)=>{
+  const token = "xyz";
+  const isAdminAuthorized= token ==="xyz";
+  if(!isAdminAuthorized){
+   res.status(401).send("UnAuthorized Request");
+  }
+  else{
+    next();
+  } 
+})
+
+
 app.get("/admin/getAllData",(req,res)=>{
-  //Logic if the request is authorized 
-  res.send("All Data is sent")
+    res.send("All Data Sent");
 })
 
-app.get("/admin/deleteUserData",(req,res)=>{
-  // Logic of checking if the user is authorized 
-})
-
-
-
-
-
+app.get("/admin/deleteUser",(req,res)=>{
+   res.send("Deleted a user");
+  }) 
 
 app.listen(7777,()=>{
   console.log("Server is successfully listening on port 7777...")
